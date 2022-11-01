37622a1c2784032422acb65addfa3d9eeb22c417
bete7512 <betekbebe@gmail.com>
2022-11-01 09:41:13 +0300

    change commited succefully

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -9,4 +9,5 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        response = number_of_subscribers(sys.argv[1])
+        print(response)
