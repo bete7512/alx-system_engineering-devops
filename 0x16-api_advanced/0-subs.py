89584652b91fb05b7631ffcdeeff4e05ba1867ee
bete7512 <betekbebe@gmail.com>
2022-11-01 09:53:18 +0300

    change commited succefully

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ import requests
 def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Linux/client/0.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=headers,allow_redirects=False)
-    if not response.status_code == 404:
+    response = requests.get(url, headers=headers)
+    if response.status_code == 404:
         return 0
     else:
-        result = response.json().get('data').get('subscriber')
-        return 
+        result = response.json().get('data')
+        return result.get('subscribers')
