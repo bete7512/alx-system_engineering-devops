5c19bdc1b8d9a94e1306cf6c7bf14d30ec8a2586
bete7512 <betekbebe@gmail.com>
2022-11-01 09:55:01 +0300

    change commited succefully

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """"advanced api oth tasks"""
-from unittest import result
 import requests
 
 
 def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Linux/client/0.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, )
     if response.status_code == 404:
         return 0
     else:
-        result = response.json().get('data')
-        return result.get('subscribers')
+        result = response.json()['data']['subscribers']
+        return result
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""advanced api oth tasks"""
+import requests
+
+
+def top_ten(subreddit):
+    headers = {'User-Agent': 'Linux/client/0.0'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 404:
+        return 0
+    else:
+        result = response.json().get('data')
+        return result.get('subscribers')
