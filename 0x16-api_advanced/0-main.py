f7f102cdd22a200019c4f7e669709391997d3a07
bete7512 <betekbebe@gmail.com>
2022-11-01 09:48:05 +0300

    change commited succefully

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -5,6 +5,8 @@
 import sys
 
 if __name__ == '__main__':
+    username = input("Enter username:")
+    print("Username is: " + username)
     number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
