af0d89b7d7c5e7d91af81e307c9b7c804dd89a95
bete7512 <betekbebe@gmail.com>
2022-11-01 09:36:04 +0300

    change commited succefully

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,9 +2,9 @@
 """"advanced api oth tasks"""
 import requests
 def number_of_subscribers(subreddit):
-    headers = {'User-agent':'Linux/client/0.0'}
+    headers = {'User-Agent':'Linux/client/0.0'}
     response = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers)
     if not response:
         return 0
-    else 
-    return response
+    else:
+        return response
