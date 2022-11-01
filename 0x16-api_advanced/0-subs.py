55820fb09f5d0fa50cf1dc9983312b93f8b3ce01
bete7512 <betekbebe@gmail.com>
2022-11-01 09:39:30 +0300

    change commited succefully

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Linux/client/0.0'}
-    response = requests.get(
-        'https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers,allow_redirects=False)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=headers,allow_redirects=False)
     if not response:
         return 0
     else:
