2cabb316cd1a6d1720949f1444dd55459c62a5a0
bete7512 <betekbebe@gmail.com>
2022-11-01 11:26:01 +0300

    change commited succefully

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,8 +8,12 @@ def recurse(subreddit, hot_list=[]):
     headers = {'User-Agent': 'Linux/client/0.0'}
     url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
     response = requests.get(url, headers=headers)
+    if response.status_code == 404:
+        return None
     count = 0
-    data_list = response['data']['children'][count]
-    count++
+    data_list = []
+    data_list.append(response['data']['children'][count])
+    count = count + 1
+    return data_list
 
     
