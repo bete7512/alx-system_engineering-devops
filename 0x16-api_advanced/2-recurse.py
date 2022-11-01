2909d3f2a9c98cf989571078747f17cc0f91f2a1
bete7512 <betekbebe@gmail.com>
2022-11-01 15:25:53 +0300

    change commited succefully

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,20 +6,17 @@ import requests
 def recurse(subreddit, hot_list=[]):
     """"Alx verification of editing tools"""
     headers = {'User-Agent': 'Linux/client/0.0'}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    response = requests.get(url, headers=headers,allow_redirects=False)
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+    response = requests.get(url, headers=headers)
     if response.status_code == 404:
         return None
     result = response.json()['data']['children']
     max_count = len(result)
     count = len(hot_list)
-    if len(hot_list) >= max_count:
-        return
-    if hot_list == []:
+    if len(hot_list) == 0:
         count = 0
-    # else:
-    #     count = count +1
-    print(count)
+    else:
+        count = count +1
     if count < max_count:
         hot_list.append(result[count])
         recurse(subreddit,hot_list)
