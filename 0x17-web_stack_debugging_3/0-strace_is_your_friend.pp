a4b20f69ba3a705ece6f3ca61f46406efc605fbb
bete7512 <betekbebe@gmail.com>
2022-11-02 18:31:17 +0300

    change commited succefully

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Create a manifest that fixes file name typo
+# fix problems for apache
 exec { 'fix_typo':
   command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
   path    => '/bin/'
